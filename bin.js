2ec86dee17233a846e524db0066332c6a6c96ae6
Eric Bower <neurosnap@gmail.com>
2017-03-28 11:06:06 -0400

    weird

--- bin.js
+++ bin.js
@@ -42,8 +42,8 @@ var execPath = process.env.npm_execpath || process.env.NPM_CLI_JS
 if (util.isYarnPath(execPath) && /node_modules/.test(process.cwd())) {
   // From yarn repository
 } else if (!(typeof pkg._from === 'string')) {
-  log.info('install', 'installing inside prebuild-install directory, skipping download.')
-  process.exit(1)
+  // log.info('install', 'installing inside prebuild-install directory, skipping download.')
+  // process.exit(1)
 } else if (pkg._from.length > 4 && pkg._from.substr(0, 4) === 'git+') {
   log.info('install', 'installing from git repository, skipping download.')
   process.exit(1)
